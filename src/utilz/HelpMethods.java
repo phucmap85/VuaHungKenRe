9987292c651bec137319982f56a53134674aecf5
Le Quang Phuc <phuclequang2006@gmail.com>
2025-10-05 11:21:44 +0700

    Refactor code

--- src/utilz/HelpMethods.java
+++ src/utilz/HelpMethods.java
@@ -3,8 +3,8 @@ package utilz;
 import java.awt.Rectangle;
 
 public class HelpMethods {
-    public static boolean canMoveHere(Rectangle r){
-        if(r.x  < 0 || r.x + r.width > 1143 || r.y  + r.height > 600 || r.y  < 0) return false;
+    public static boolean canMoveHere(Rectangle r) {
+        if (r.x  < 0 || r.x + r.width > 1143 || r.y  + r.height > 600 || r.y  < 0) return false;
         return true;
     }
 }
